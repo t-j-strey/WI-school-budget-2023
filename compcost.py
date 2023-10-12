589e9ab8ea585ff2b74d78b2050255708e53539e
Tom <tjstrey@gmail.com>
2023-10-11 22:16:57 -0500

    generating a blank XLSX file now...

--- compcost.py
+++ compcost.py
@@ -8,7 +8,7 @@ class CompcostSpider(scrapy.Spider):
     start_urls = ["https://sfs.dpi.wi.gov/sfsdw/CompCostReport.aspx"]
 
     custom_settings = {
-        'FEEDS' : {'data/%(name)s/%(name)s_%(time)s.csv': {'format':'csv','overwrite':True}}
+        #'FEEDS' : {'data/%(name)s/%(name)s_%(time)s.csv': {'format':'csv','overwrite':True}}
     }
 
     def parse(self, response):
@@ -25,19 +25,18 @@ class CompcostSpider(scrapy.Spider):
         yield FormRequest.from_response(response,
                                   formdata=formdata,
                                   headers=headers,
+                                  callback=self.datacapture)
                                   #callback=self.tab2)
-                                  callback = self.datacapture)
+        
         
        
     def datacapture(self,response):
+        #dfs = pd.read_html(response.text)
+        dfs = pd.read_html(response,match='Data For Comparative Cost Per Member')
         #dfs = pd.read_html(response,attrs={'id':'ct100_MainContent_Grid_StandardReport'})
-        match = "Data For Comparative Cost Per Member"
-        dfs = pd.read_html(response,match = match)
         writer = pd.ExcelWriter('data.xlsx',engine='xlsxwriter')
-        dfs[1].to_excel('data.xlsx',index = False, header = True)
-        #for i, df in enumerate(dfs):
-        #    if i == 3: 
-        #        df.to_csv(f'data_{i}.csv')
+        dfs[0].to_excel(writer,index = False, header = True)
+       
 
     
 
