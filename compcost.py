a6e51def9345ae97d78c178e69fc3b47f1ed6bf2
Tom Strey <tjstrey@mmm.com>
2023-10-12 16:24:16 -0500

    now broken, trying to loop through all years and add to single dataframe

--- compcost.py
+++ compcost.py
@@ -11,39 +11,54 @@ class CompcostSpider(scrapy.Spider):
     custom_settings = {
         #'FEEDS' : {'data/%(name)s/%(name)s_%(time)s.csv': {'format':'csv','overwrite':True}}
     }
-
-    def parse(self, response):
-        headers = {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        }
-
-        formdata= {
-            'ctl00$MainContent$selFiscalYear' : '2021',
-            'ctl00$MainContent$selSchoolDist' : '0007',
-            'ctl00$MainContent$rbOrderListBy':	'Name',
-            'ctl00$MainContent$btnSubmitAllAgencies':	'Show+All+Agencies',
-        }
-        yield FormRequest.from_response(response,
-                                  formdata=formdata,
-                                  headers=headers,
-                                  callback=self.datacapture)
-                                  #callback=self.tab2)
+    for yrs in range(2020,2021): 
+        stryears = str(yrs)
         
-        
-       
+        def parse(self, response):
+            headers = {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+            
+            formdata= {
+                'ctl00$MainContent$selFiscalYear' : self.stryears,
+                'ctl00$MainContent$selSchoolDist' : '0007',
+                'ctl00$MainContent$rbOrderListBy':	'Name',
+                'ctl00$MainContent$btnSubmitAllAgencies':	'Show+All+Agencies',
+            }
+            #yield FormRequest.from_response(response,
+            results = FormRequest.from_response(response,
+                                    formdata=formdata,
+                                    headers=headers,
+                                    callback=self.datacapture)
+                                    #callback=self.tab2)
+
+            print(results)
+            
+
+
     def datacapture(self,response):
         dfs = pd.read_html(response.text)
+        df = dfs[3]
+        yield df
+
+     #   length = len(df.index)
+     #   yrdata = np.full((length),self.yrs)
+     #   df.insert(1,"Year",yrdata)
         #dfs = pd.read_html(response,attrs={'id':'ctl00_MainContent_Grid_StandardReport'})
-        with pd.ExcelWriter(
-            "data.xlsx",
-            engine = 'xlsxwriter'
-        ) as writer:
-            dfs[3].to_excel(writer,
-                            sheet_name='Comp Cost Per Member',
-                            index = False, 
-                            header = True, 
-                            merge_cells=True
-                            )
+        
+        
+    #with pd.ExcelWriter(
+    #    "CompCostPerMember.xlsx",
+    #    engine = 'openpyxl',
+    #    mode ='w'
+    #) as writer:
+    #    self.results.to_excel(writer,
+    #                    sheet_name='Comp Cost Per Member',
+    #                    index = False, 
+    #                    header = True, 
+    #                    merge_cells=True
+    #                    )
+           
 
         
 
