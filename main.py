567e9ec1a7259aeac88fe4f8a97fb88f87c76500
Tom Strey <tjstrey@mmm.com>
2023-10-13 12:58:03 -0500

    add minimal main structure

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('Hello World')
+import compcost
+import scrapy
+from scrapy.crawler import CrawlerProcess
+
+
+print('Run Scraper')
+
+process = CrawlerProcess(
+    settings={
+        
+    }
+)
+
+#process.crawl()
+
