b1f9d61c2c705d79fc2b60eef0afa6f8be81c407
Tom Strey <tjstrey@mmm.com>
2023-10-17 14:04:35 -0500

    added list of reports and years. built loop mechanism to call scraper iteratively over defined reports and years

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ from WICompCost.WICompCost.spiders.compcost import CompcostSpider
 from scrapy.crawler import CrawlerRunner
 from scrapy.utils.project import get_project_settings
 
+std_reports = ["https://sfs.dpi.wi.gov/sfsdw/CompCostReport.aspx",
+               "https://sfs.dpi.wi.gov/sfsdw/CompRevReport.aspx"]
+start_year = 2018
+end_year = 2019 #inclusive
+
 
 def main():
     configure_logging()
@@ -14,9 +19,10 @@ def main():
 
     @defer.inlineCallbacks
     def crawl():
-        yield runner.crawl(CompcostSpider,year = "2019",stdreport="https://sfs.dpi.wi.gov/sfsdw/CompCostReport.aspx")
-        #yield runner.crawl(ComprevSpider)
-        #add more spiders here
+        for report in std_reports:  #loop through the list of available standard reports
+            for i in range(start_year,end_year + 1):  #loop through range of years            
+                yield runner.crawl(CompcostSpider,year = str(i),stdreport= report)
+                #add more spiders here
         reactor.stop() # type: ignore
 
     crawl()
