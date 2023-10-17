d71c5a9341b4974b7c381559f1501c13f917d6ac
Tom Strey <tjstrey@mmm.com>
2023-10-17 16:00:58 -0500

    next attempt at pulling scraped dictionary from spider into main.  Original calls are commented out, spider_output function defined, not working

--- main.py
+++ main.py
@@ -4,31 +4,60 @@ from twisted.internet import task, reactor, defer
 from WICompCost.WICompCost.spiders.compcost import CompcostSpider
 from scrapy.crawler import CrawlerRunner
 from scrapy.utils.project import get_project_settings
+from scrapy.signalmanager import dispatcher 
+from scrapy import signals
 
-std_reports = ["https://sfs.dpi.wi.gov/sfsdw/CompCostReport.aspx",
-               "https://sfs.dpi.wi.gov/sfsdw/CompRevReport.aspx"]
+
+std_reports = ["https://sfs.dpi.wi.gov/sfsdw/CompCostReport.aspx"]
+              # "https://sfs.dpi.wi.gov/sfsdw/CompRevReport.aspx"]
 start_year = 2018
-end_year = 2019 #inclusive
+end_year = 2018 #inclusive
 
 
-def main():
-    configure_logging()
-    settings = get_project_settings()
-    runner = CrawlerRunner(settings)
+#def main():
+
+    #configure_logging()
+    #settings = get_project_settings()
+    #runner = CrawlerRunner(settings)
     
 
-    @defer.inlineCallbacks
-    def crawl():
-        for report in std_reports:  #loop through the list of available standard reports
-            for i in range(start_year,end_year + 1):  #loop through range of years            
-                yield runner.crawl(CompcostSpider,year = str(i),stdreport= report)
-                #add more spiders here
-        reactor.stop() # type: ignore
+    #@defer.inlineCallbacks
+    #def crawl():
+    #    for report in std_reports:  #loop through the list of available standard reports
+    #        for i in range(start_year,end_year + 1):  #loop through range of years            
+    #            yield runner.crawl(CompcostSpider,year = str(i),stdreport= report)
+    #            #add more spiders here
+    #    reactor.stop() # type: ignore
 
+    #crawl()
+    #reactor.run() # type: ignore
+
+
+def spider_output(spider):
+    output = []
+
+    def get_output(item):
+        output.append(item)
+
+    dispatcher.connect(get_output, signal=signals.item_scraped)
+
+    settings = get_project_settings()
+    runner = CrawlerRunner(settings)
+    #@defer.inlineCallbacks
+    def crawl():
+        runner.crawl(spider,"2019","https://sfs.dpi.wi.gov/sfsdw/CompCostReport.aspx")
+        yield output
+        reactor.stop() #type: ignore
     crawl()
-    reactor.run() # type: ignore
+    reactor.run() #type:ignore
+    
 
 
 # this only runs if the module was *not* imported
 if __name__ == "__main__":
-    main()
+    #main()
+
+    spider = 'CompcostSpider'
+    response = spider_output(spider)
+    
+    print(response)
