23e97195eb71589fb503404521503b4829f462a2
Tom Strey <tjstrey@mmm.com>
2023-10-20 23:17:52 -0500

    added new ditrict profiles program

--- dist_profiles/dist_profiles/spiders/dist_profiles_spdr.py
+++ dist_profiles/dist_profiles/spiders/dist_profiles_spdr.py
@@ -1,10 +1,19 @@
 import scrapy
+from scrapy import FormRequest,Request
+import pandas as pd
+import numpy as np
+import io
+import logging
 
 
 class DistProfilesSpdrSpider(scrapy.Spider):
     name = "dist_profiles_spdr"
     allowed_domains = ["sfs.dpi.wi.gov"]
     start_urls = ["https://sfs.dpi.wi.gov/sfsdw/Agency_Financial_profile.aspx"]
+
+    def __init__(self,year=None, stdreport="", *args, **kwargs):
+        super(DistProfilesSpdrSpider,self).__init__(*args,**kwargs)
+        logging.getLogger('scrapy').setLevel(logging.WARNING)
 
     def parse(self, response):
         pass
--- dist_profiles_crawl.py
+++ dist_profiles_crawl.py
@@ -1,47 +1,37 @@
 
-from scrapy.utils.log import configure_logging
 from twisted.internet import reactor, defer
-from std_reports.std_reports.spiders.std_reports_spider import StdReportsSpider
-from std_reports.std_reports import pipelines as std_report_pipeline
+from dist_profiles.dist_profiles.spiders.dist_profiles_spdr import DistProfilesSpdrSpider
+from dist_profiles.dist_profiles import pipelines as dist_profiles_pipeline
 from scrapy.crawler import CrawlerRunner
 from scrapy.utils.project import get_project_settings
 import os
 import pandas as pd
 from excel_utils import create_title, export_workbook
+from scrapy.utils.log import configure_logging
+
 
 PROJECT_ROOT =  os.path.dirname(os.path.abspath(__file__))
 os.chdir(PROJECT_ROOT)
 
-
-std_reports = ["https://sfs.dpi.wi.gov/sfsdw/CompCostReport.aspx",
-               #"https://sfs.dpi.wi.gov/sfsdw/CompRevReport.aspx",
-               #"https://sfs.dpi.wi.gov/sfsdw/EqualizationAidReport.aspx",
-               #"https://sfs.dpi.wi.gov/sfsdw/EqualizedValueReport.aspx",
-               #"https://sfs.dpi.wi.gov/sfsdw/MembershipFTEReport.aspx",
-               #"https://sfs.dpi.wi.gov/sfsdw/MillRateReport.aspx",
-               "https://sfs.dpi.wi.gov/sfsdw/RevenueLimitReport.aspx"]
-start_year = 2004
-end_year = 2005 #inclusive
-
 def main():
-    
+
     configure_logging(
         {"LOG_LEVEL":"INFO"}
     )
 
-    settings_file_path = 'std_reports.std_reports.settings'   #Relative Location of Settings File
+    settings_file_path = 'dist_profiles.dist_profiles.settings'   #Relative Location of Settings File
     os.environ.setdefault('SCRAPY_SETTINGS_MODULE',settings_file_path)
     settings = get_project_settings()
     runner = CrawlerRunner(settings)
 
-    @defer.inlineCallbacks
+    @defer.inlineCallbacks #type: ignore
     def crawl():
         for report in std_reports:  #loop through the list of available standard reports
             
             for i in range(start_year,end_year + 1):  #loop through range of years            
-                yield runner.crawl(StdReportsSpider,year = str(i),stdreport= report)
+                yield runner.crawl(DistProfilesSpdrSpider)
                 #add more spiders here
-
+        
             comp_df = pd.DataFrame()
             for item in std_report_pipeline.items:
                 df = pd.DataFrame.from_dict(item)
@@ -50,25 +40,17 @@ def main():
                     df = df.drop(index = [0,1])
                 comp_df = pd.concat([comp_df,df])
             std_report_pipeline.items.clear()
-
-
-
-            
-
-
+        
             title = create_title(report) #create a Spreadsheet title from site address
             export_workbook(title,comp_df)# take dataframe and turn it into a formatted Excel Workbook
 
         reactor.stop() # type: ignore
         
     crawl()
+     
     reactor.run() # type: ignore
 
-   
-    
-
-
-# this only runs if the module was *not* imported
+ #this only runs if the module was *not* imported
 if __name__ == "__main__":
     main()
-
+    
