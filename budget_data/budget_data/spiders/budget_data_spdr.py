45b8e638a1dfbb4e7eda21d60ae1ca531230dcbe
Tom Strey <tjstrey@mmm.com>
2023-10-29 15:50:19 -0500

    simplified spider to single loop looking for multiple keywords, added keyword for COA description

--- budget_data/budget_data/spiders/budget_data_spdr.py
+++ budget_data/budget_data/spiders/budget_data_spdr.py
@@ -13,19 +13,10 @@ class BudgetDataSpdrSpider(scrapy.Spider):
 
     def parse(self, response):
         for result1 in response.xpath('//div[@class="template-inner"]'):
-            for result2 in result1.xpath('.//a/@href[(contains(., "Descriptions"))]'): #returns Account Descriptions
+            for result2 in result1.xpath('.//a/@href[(contains(., "Descriptions")) or (contains(., "COA")) or (contains(., "AtoZ"))]'): #returns Account Descriptions
                 file_url =  "https://dpi.wi.gov/" + result2.extract()
-                #print("\nDescriptions URLs: ", file_url)
                 item = BudgetDataItem()
                 item['file_urls'] = [file_url]
                 yield item
 
-            for result3 in result1.xpath('.//a/@href[(contains(., "AtoZ"))]'): #returns only AtoZ URLs
-                print("\nBudget Data: ",result3.extract())
-                file_url =  "https://dpi.wi.gov/" + result3.extract()
-                item = BudgetDataItem()
-                item['file_urls'] = [file_url]
-                yield item
-            
-    
         #inspect_response(response,self)
