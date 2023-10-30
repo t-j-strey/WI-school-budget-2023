671f019519f4693efe1693558d26b948e0934831
Tom Strey <tjstrey@mmm.com>
2023-10-29 21:03:55 -0500

    formatted non-conforming link namesautomatically so all filenames now have consistent formats

--- budget_data/budget_data/pipelines.py
+++ budget_data/budget_data/pipelines.py
@@ -23,7 +23,18 @@ class BudgetDataPipeline(FilesPipeline):
    def file_path(self, request, response=None, info=None, *, item=None):
       media_guid = request.url
       file_name = media_guid.rsplit('/',1)[1]
-      print("\n\nFile Name: ",file_name)
+      #2013 filepath starts with random 'd' for some reason, this removes it
+      if file_name.startswith('d'):
+         file_name = file_name[1:]
+      #from 2018 and newer, descriptions filename and type changes.  below checks for new
+      #filename, and change it to match the rest of the files before saving
+      if "COA" in file_name:
+         file_name= file_name.split('_',1)[1]
+         #capture first year, which matches first 4 characters
+         year = int(file_name[:4])
+         next_year = year+1
+         file_name = str(year) + "-"+ str(next_year) +"-01-Budget-Accout-Descriptions.xlsx"
+         print("\n\n",file_name)
       return f"full/{file_name}"
    
    #def process_item(self, item, spider):
